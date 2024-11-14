9504d32e1ad3777e4de197afc5f7a9133319294c
oulkhorfmohamed@gmail.com <moulkhorf@bar.sio.fr>
2024-11-14 13:31:50 +0100

    Projet base

---
+++ src/main/java/bar/sio/camping/CampingApplication.java
@@ -0,0 +1,13 @@
+package bar.sio.camping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampingApplication.class, args);
+	}
+
+}
